6f9d4a72f9694325f48cdeda9ab9b40807e54608
JIEUN LEE <kate@jieuns-mbp.home>
2018-03-28 13:51:03 +0100

    create-initial-application- Add search methods to repositories

--- src/main/java/com/rodrigo/review/ReviewRepository.java
+++ src/main/java/com/rodrigo/review/ReviewRepository.java
@@ -1,6 +1,17 @@
 package com.rodrigo.review;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-public interface ReviewRepository extends CrudRepository<Review, Long> {
+public interface ReviewRepository extends PagingAndSortingRepository<Review, Long> {
+
+    @RestResource(rel = "rating-is-equal-to", path = "ratingEqualsTo")
+    Page<Review> findByRating(@Param("rating") int rating, Pageable page);
+
+    @RestResource(rel = "find-by-description-contains", path = "descriptionContains")
+    Page<Review> findByDescriptionContains(@Param("description") String description, Pageable page);
 }
