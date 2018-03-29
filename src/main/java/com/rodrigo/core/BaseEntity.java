3bc9cc09dae99a985a6a93a338b1e1571d7576ce
JIEUN LEE <kate@jieuns-mbp.home>
2018-03-29 17:24:19 +0100

    create-initial-application Add validation

--- src/main/java/com/rodrigo/core/BaseEntity.java
+++ src/main/java/com/rodrigo/core/BaseEntity.java
@@ -1,9 +1,6 @@
 package com.rodrigo.core;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 
 @MappedSuperclass
 public abstract class BaseEntity {
@@ -11,6 +8,9 @@ public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private final Long id;
+
+    @Version
+    private Long version;
 
     protected BaseEntity() {
         id = null;
